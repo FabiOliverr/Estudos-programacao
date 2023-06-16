510d52217313be909dc04bba2d6eae1e1d75dcbf
FabiOliverr <bhalfur@gmail.com>
2023-06-16 18:57:08 -0300

    feito aula sobre comentarios

---
+++ comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo na tela do computador!//
+
+console.log('passo #1')// mais um exemplo de console.log!
+
+/*
+Esse é um 
+comentario 
+de múltiplas 
+linhas!
+*/
