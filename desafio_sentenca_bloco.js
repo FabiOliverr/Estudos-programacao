5293b5f47cd35c7da9a50fbf96599d43f921f2bd
FabiOliverr <bhalfur@gmail.com>
2023-06-17 05:06:52 -0300

    adicionado primeiro desafio e concluido

---
+++ desafio_sentenca_bloco.js
@@ -0,0 +1,41 @@
+/*
+
+// Bloco 1
+numeros de 
+1
+3
+5
+7
+9
+
+// Bloco 2 
+numeros de 
+2
+4
+6
+8
+10
+
+
+*/
+
+let bloco_1 = 1;
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+let bloco_2 = 2;
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+
+
+
