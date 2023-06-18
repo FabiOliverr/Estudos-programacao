e6d7a488db0c98882cf98ba85ad826b128d90d92
FabiOliverr <bhalfur@gmail.com>
2023-06-18 03:31:44 -0300

    feito aula sobre dados, aaprendendo a usar uma variavel e como ela tem impacto no console.log e no bloco de codigos

--- Dados.js
+++
@@ -1,3 +0,0 @@
-console.log("Caneta");
-console.log(10);
-console.log(64);
---
+++ Dados_1.js
@@ -0,0 +1,18 @@
+
+// *"var" pode ser usando para criar uma variável.*Pensamos nela como se fosse uma caixa onde posso nomea-la e armazenar um valor.*Onde o computador pode armazenar um dado.*Exemplo var = criação de variavel "quantidade" = nomeando a variavel e "=" dando valor a variavel.
+
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto ;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta Bic"
+console.log(nome);
+
