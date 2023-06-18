07988b0d48484f7b269e428173bd7dd8b8287e3e
FabiOliverr <bhalfur@gmail.com>
2023-06-18 12:07:44 -0300

    aprendendo a usar string,booleano, numbers e typeof

--- Dados_2.js
+++ Dados_2.js
@@ -1,9 +1,9 @@
 let preco = 19.90;
-let desconto = 0.4;
+let desconto = 0.4;// -> O numero " 0.4" simboliza um numero de casas decimais ! 
 let precoComDesconto = preco * (1 - desconto);
 
 console.log(19.9 * 0.6);
-console.log(preco * (1 - desconto));
+console.log(preco * (1 - desconto));// -> o numero 1 simboliza um numero " literal"
 console.log(precoComDesconto);
 
 let nome = "Caderno" // String -> Sequencia de simbolos.
@@ -12,3 +12,4 @@ console.log("produto:" + nome
     + ", Categoria:" + categoria
     + ", Preço:" + preco
     + ",Descontos:" + desconto);// concatenação de codigo//
+
---
+++ Dados_3.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31);//-> "typeof" utilizado para saber o tipo de valor atribuido a variavel.
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true // ou false 
+console.log( typeof estaChovendo);
+
+console.log(typeof "Teste");
