daa43f083ea4d7bafd25c3e2a3183ed5366d1b4c
FabiOliverr <bhalfur@gmail.com>
2023-06-18 11:53:58 -0300

    finalizando dados 2, com aprendizado em concatenação e strings

--- Dados_2.js
+++ Dados_2.js
@@ -1,7 +1,14 @@
 let preco = 19.90;
 let desconto = 0.4;
-let precoComDesconto = preco * (1- desconto);
+let precoComDesconto = preco * (1 - desconto);
 
 console.log(19.9 * 0.6);
 console.log(preco * (1 - desconto));
 console.log(precoComDesconto);
+
+let nome = "Caderno" // String -> Sequencia de simbolos.
+let categoria = "Papelaria";
+console.log("produto:" + nome
+    + ", Categoria:" + categoria
+    + ", Preço:" + preco
+    + ",Descontos:" + desconto);// concatenação de codigo//
