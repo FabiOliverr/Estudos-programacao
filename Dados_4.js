a5bbbeb1b47c95b18fa79667b3fe7e07f245d07c
FabiOliverr <bhalfur@gmail.com>
2023-06-19 15:43:37 -0300

    aprendendo o uso de const e seu significado.

---
+++ Dados_4.js
@@ -0,0 +1,17 @@
+const a = 3;
+
+//....
+
+a = 10;// -> se voce colocar a declaração com um novo valor, ele entendera que substituirá o valor antigo.
+
+//....
+
+console.log(a);
+
+a = a + 10; 
+
+console.log(a);
+
+// como transformar a variavel "a" em uma constante ? pode-se usar " const" significa que nao conseguirei mais mudar o valor de "a".
+
+//dica: " crie mais constantes que variáveis"!!
