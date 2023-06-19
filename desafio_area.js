0dee8440cc88f94c0301cf0dd9bb3ade0017e1c2
FabiOliverr <bhalfur@gmail.com>
2023-06-19 16:11:51 -0300

    usando const no primeiro desafio de area e fazendo um experiemnto para um novo exemplo

---
+++ desafio_area.js
@@ -0,0 +1,17 @@
+//desafio------//
+// PI * raio*raio//
+
+
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + " m2");
+
+
+// experimentos feitos por mim//
+const feijao = 3.75
+let CaixaFeijao = 15 
+let ValorProdutoComposto = feijao * CaixaFeijao;
+
+console.log("o Valor de uma caixa com 12 unidades de capotes de feijao é " + ValorProdutoComposto);
