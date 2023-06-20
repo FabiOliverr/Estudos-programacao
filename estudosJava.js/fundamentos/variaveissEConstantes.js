c45d627787b82334fe0975a975a9ee2b5da729ba
FabiOliverr <bhalfur@gmail.com>
2023-06-20 15:05:47 -0300

    aprendendo sobre var,let&const e suas atribuiçoes

---
+++ estudosJava.js/fundamentos/variaveissEConstantes.js
@@ -0,0 +1,25 @@
+
+/* var = variável ou declarador 
+
+* "a" = identificador da variável ou seja nome que eu atribuo 
+
+ * "=" ele é o simbolo de atribuição
+
+* e o "3" é o valor atribuido a sentença de codigo
+
+*/
+
+var a = 3;
+let b = 4
+
+var a = 30;
+b = 40;
+console.log(a, b);
+
+a = 300;
+b = 400;
+console.log(a, b);
+
+const c = 5;
+// c = 50
+console.log(c);
