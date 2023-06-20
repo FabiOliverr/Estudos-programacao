35eb350599beb6e0b5a598bb0f7d4f118693ad3f
FabiOliverr <bhalfur@gmail.com>
2023-06-20 14:48:11 -0300

    falando sobre comentarios e suas variações

---
+++ estudosJava.js/fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// existem comentarios de uma unica linha usando " //"
+console.log("Linha 1");
+
+//existem comentarios de multiplas linhas usando            "/*" Exemplo:
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+
+console.log('Linha 2');
+
+/*
+*Comentário de
+*múltiplas linhas
+*/
