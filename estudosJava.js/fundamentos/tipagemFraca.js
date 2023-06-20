463a0282ca1289a7adbb795ef3a46ea169154de9
FabiOliverr <bhalfur@gmail.com>
2023-06-20 15:44:53 -0300

    aprendendo sobre tipagem de linguagem e sobre escolha de nomes

---
+++ estudosJava.js/fundamentos/tipagemFraca.js
@@ -0,0 +1,19 @@
+let qualquer = 'Legal'
+console.log(qualquer);
+console.log(typeof qualquer);
+
+let confuso = true;
+console.log(confuso);
+console.log(typeof confuso);
+
+let numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+// Evitar nomes genéricos e siglas para declarar uma variável. Exemplo //
+
+let valor ='';
+let numeroo = 1;
+let pqp = false // Produto Químico Perigoso... kkk
+
+//-------------------------------------------------------//
